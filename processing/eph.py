dfcdcefe929be64f850fb3b89531904b7f7a09ba
camille.chomel <camille.chomel@FOXE-FR>
2022-07-06 15:06:58 +0200

    add code before testing

---
+++ processing/eph.py
@@ -0,0 +1,38 @@
+import ephem
+from datetime import datetime, timedelta
+
+def GetRange(satellite_name, tle_line_1, tle_line_2, station_latitude, station_longitude, station_altitude, computation_date):
+    satellite = ephem.readtle(satellite_name, tle_line_1, tle_line_2)
+
+    city = ephem.Observer()
+    city.lon, city.lat, city.elevation, city.date = station_latitude , station_longitude , station_altitude, computation_date
+    satellite.compute(city)
+    
+    #print("range for date " + computation_date)
+
+    return satellite.az, satellite.alt, satellite.range
+
+# Date should have format %Y/%m/%d %H:%M:%S
+def GetPointingRangeList(satellite_name, tle_line_1, tle_line_2, station_latitude, station_longitude, station_altitude, date_start, date_stop):
+    date_pattern = '%Y/%m/%d %H:%M:%S'
+
+    datetime_start = datetime.strptime(date_start, date_pattern)
+    datetime_stop = datetime.strptime(date_stop, date_pattern)
+
+    number_of_entries = int((datetime_stop - datetime_start).total_seconds())
+    range_list = []
+    for i in range(number_of_entries):
+        current_date = datetime_start + timedelta(0,i)
+        current_date_string = current_date.strftime(date_pattern)
+
+        azimuth, elevation, range_meter = GetRange(satellite_name, tle_line_1, tle_line_2, station_latitude, station_longitude, station_altitude, current_date_string)
+        
+        range_list.append({
+            "date": current_date_string,
+            "azimuth": azimuth,
+            "elevation": elevation,
+            "range": range_meter
+        })
+    
+    return range_list
+
---
+++ processing/main.py
@@ -0,0 +1,20 @@
+from eph import GetPointingRangeList
+import sys
+
+print ('Number of arguments:', len(sys.argv))
+
+if len(sys.argv) != 9 :
+    print("Wrong number of arguments. See README.")
+
+satellite_name = sys.argv[1]
+tle_line_1 = sys.argv[2]
+tle_line_2 = sys.argv[3]
+station_latitude = float(sys.argv[4])
+station_longitude = float(sys.argv[5])
+station_altitude = float(sys.argv[6])
+date_start = sys.argv[7]
+date_stop = sys.argv[8]
+
+pointingRangeList = GetPointingRangeList(satellite_name, tle_line_1, tle_line_2, station_latitude, station_longitude, station_altitude, date_start, date_stop)
+
+print(pointingRangeList)

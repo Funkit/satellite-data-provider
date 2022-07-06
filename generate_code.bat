6f135e840f73da9e879155d2ff94654cdce63553
camille.chomel <camille.chomel@FOXE-FR>
2022-07-06 17:19:47 +0200

    proto import working

--- generate_code.bat
+++ generate_code.bat
@@ -1,1 +1,1 @@
-python -m grpc_tools.protoc -I./proto --python_out=./processing --grpc_python_out=./processing ./proto/helloworld.proto
+python -m grpc_tools.protoc -I./proto --python_out=./processing --grpc_python_out=./processing ./proto/pointing.proto

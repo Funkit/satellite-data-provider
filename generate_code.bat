1c16e3091d46d05a5f08f55ac0d1645e5e1c1e35
camille.chomel <camille.chomel@FOXE-FR>
2022-07-08 11:46:20 +0200

    working generation (was missing second proto file)

--- generate_code.bat
+++ generate_code.bat
@@ -1,2 +1,2 @@
-python -m grpc_tools.protoc -I./proto --python_out=./processing --grpc_python_out=./processing ./proto/pointing.proto
-protoc --proto_path=./proto --go_out=./api-composer/pointing --go_opt=paths=source_relative --go-grpc_out=./api-composer/pointing --go-grpc_opt=paths=source_relative proto/pointing.proto
+python -m grpc_tools.protoc -I./proto --python_out=./processing --grpc_python_out=./processing ./proto/pointing.proto ./proto/datetime.proto
+protoc --proto_path=./proto --go_out=./api-composer/pointing --go_opt=paths=source_relative --go-grpc_out=./api-composer/pointing --go-grpc_opt=paths=source_relative proto/pointing.proto ./proto/datetime.proto
---
+++ processing/pointing_server.py
@@ -0,0 +1,8 @@
+from concurrent import futures
+import logging
+
+import grpc
+import pointing_pb2
+import pointing_pb2_grpc
+
+

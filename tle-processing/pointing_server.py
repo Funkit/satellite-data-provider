288757b6d57ce977a0712ab3edcddaf54cff6a08
camille.chomel <camille.chomel@FOXE-FR>
2022-07-29 18:04:48 +0200

    basic client server working

--- tle-processing/pointing_server.py
+++ tle-processing/pointing_server.py
@@ -5,4 +5,21 @@ import grpc
 import pointing_pb2
 import pointing_pb2_grpc
 
+class Greeter(pointing_pb2_grpc.ProcessingServicer):
+
+    def GetAntennaPointing(self, request, context):
+        return pointing_pb2.AntennaPointingReply(message="Satellite name = %s" % request.satellite_information.satellite_name)
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    pointing_pb2_grpc.add_ProcessingServicer_to_server(Greeter(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    serve()
+
 

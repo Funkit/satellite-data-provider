878f7f287dda28bc7c92ef35c1f8b1a8e320993d
camille.chomel <camille.chomel@FOXE-FR>
2022-08-04 15:21:32 +0200

    Formatting

--- tle-processing/eph.py
+++ tle-processing/eph.py
@@ -2,16 +2,20 @@ import ephem
 from datetime import datetime, timedelta
 
 
-def get_range(satellite_name, tle_line_1, tle_line_2, station_latitude, station_longitude, station_altitude, computation_date):
+def get_range(satellite_name, tle_line_1, tle_line_2,
+              station_latitude, station_longitude, station_altitude,
+              computation_date):
     satellite = ephem.readtle(satellite_name, tle_line_1, tle_line_2)
 
     city = ephem.Observer()
-    city.lon, city.lat, city.elevation, city.date = station_latitude , station_longitude , station_altitude, computation_date
+    city.lon, city.lat, city.elevation, city.date = station_latitude, station_longitude, station_altitude, computation_date
     satellite.compute(city)
     return satellite.az, satellite.alt, satellite.range
 
 
-def get_pointing_range_list(satellite_name, tle_line_1, tle_line_2, station_latitude, station_longitude, station_altitude, date_start, date_stop):
+def get_pointing_range_list(satellite_name, tle_line_1, tle_line_2,
+                            station_latitude, station_longitude, station_altitude,
+                            date_start, date_stop):
     date_pattern = '%Y/%m/%d %H:%M:%S'
 
     datetime_start = datetime.strptime(date_start, date_pattern)
@@ -20,10 +24,12 @@ def get_pointing_range_list(satellite_name, tle_line_1, tle_line_2, station_lati
     number_of_entries = int((datetime_stop - datetime_start).total_seconds())
     range_list = []
     for i in range(number_of_entries):
-        current_date = datetime_start + timedelta(0,i)
+        current_date = datetime_start + timedelta(0, i)
         current_date_string = current_date.strftime(date_pattern)
 
-        azimuth, elevation, range_meter = get_range(satellite_name, tle_line_1, tle_line_2, station_latitude, station_longitude, station_altitude, current_date_string)
+        azimuth, elevation, range_meter = get_range(satellite_name, tle_line_1, tle_line_2,
+                                                    station_latitude, station_longitude, station_altitude,
+                                                    current_date_string)
         
         range_list.append({
             "date": current_date_string,
@@ -33,4 +39,3 @@ def get_pointing_range_list(satellite_name, tle_line_1, tle_line_2, station_lati
         })
     
     return range_list
-

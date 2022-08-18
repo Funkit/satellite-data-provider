8406a4cc22cd4a53568089a30c2a8e007d1a3707
camille.chomel <camille.chomel@FOXE-FR>
2022-08-18 13:50:12 +0200

    refactor in progress + function documentation

--- tle-processing/eph/groundstation.py
+++ tle-processing/eph/groundstation.py
@@ -5,16 +5,20 @@ from . import utils
 
 class Station:
     def __init__(self, name: str, latitude: float, longitude: float, altitude: float,
-                 minimum_elevation: float = 0, positioning_timeout_sec: int = 0):
+                 minimum_elevation_degrees: float = 0, positioning_timeout_sec: int = 0):
         self.name = name
         self.obs = ephem.Observer()
         self.obs.lat = latitude
         self.obs.lon = longitude
         self.obs.elevation = altitude
-        self.minimum_elevation = minimum_elevation
+        self.minimum_elevation = minimum_elevation_degrees
         self.positioning_timeout_sec = positioning_timeout_sec
 
     def next_pass(self, satellite, reference_date: datetime) -> list:
+        """
+        Returns a list containing the date, azimuth, elevation and range per second for next visibility window.
+        """
+
         output = []
 
         self.obs.date = reference_date
@@ -31,20 +35,23 @@ class Station:
             current_date_string = current_date.strftime(utils.PYEPHEM_DATE_PATTERN)
             self.obs.date = current_date_string
             satellite.compute(self.obs)
-            if utils.radians_to_degrees(satellite.alt) > self.minimum_elevation:
-                output.append({
-                    "date": current_date_string,
-                    "azimuth": satellite.az,
-                    "elevation": satellite.alt,
-                    "range": satellite.range
-                })
+            output.append({
+                "date": current_date_string,
+                "azimuth": satellite.az,
+                "elevation": satellite.alt,
+                "range": satellite.range
+            })
 
         return output
 
-    def next_available_pass(self, satellite, reference_date: datetime, end_date: datetime,
+    def next_available_pass(self, satellite, start_date: datetime, end_date: datetime,
                             minimum_pass_time_sec: int = 10) -> list:
+        """
+        Returns the next availability window between start_date and end_date where
+        there are at least minimum_pass_time_sec position samples with elevation above self.minimum_elevation
+        """
 
-        delta = int((end_date - reference_date).total_seconds())
+        delta = int((end_date - start_date).total_seconds())
 
         if delta < 0:
             raise ValueError("end_date cannot be before reference_date")
@@ -52,24 +59,58 @@ class Station:
         if delta > utils.SECONDS_IN_DAY:
             raise ValueError("maximum allowed time delta between end_date and reference_date is 24 hours")
 
-        ref_date = reference_date
-        while int((end_date - ref_date).total_seconds()) > 0:
-            self.obs.date = ref_date
-            satellite_pass = self.obs.next_pass(satellite)
+        current_date = start_date
+        while int((end_date - current_date).total_seconds()) > 0:
+            self.obs.date = current_date
+            satellite_pass = self.next_pass(satellite, current_date)
 
-            if utils.radians_to_degrees(satellite_pass[3]) > self.minimum_elevation:
-                sat_pass = self.next_pass(satellite, ref_date)
-                if len(sat_pass) > minimum_pass_time_sec:
-                    return sat_pass
+            available_pass = [item for item in satellite_pass if
+                              item['elevation'] > self.minimum_elevation]
 
-            ref_date = satellite_pass[4].datetime()
+            if len(available_pass) >= minimum_pass_time_sec:
+                return available_pass
+
+            current_date = satellite_pass[4].datetime()
 
         return []
 
-    def next_available_passes(self, satellites, reference_date: datetime, end_date: datetime,
-                              minimum_pass_time_sec: int = 10) -> dict:
+    def all_available_passes(self, satellite, start_date: datetime, end_date: datetime,
+                             minimum_pass_time_sec: int = 10) -> list:
+        """
+        Returns all availability windows between start_date and end_date where
+        there are at least minimum_pass_time_sec position samples with elevation above self.minimum_elevation
+        """
 
-        delta = int((end_date - reference_date).total_seconds())
+        delta = int((end_date - start_date).total_seconds())
+
+        if delta < 0:
+            raise ValueError("end_date cannot be before reference_date")
+
+        if delta > utils.SECONDS_IN_DAY:
+            raise ValueError("maximum allowed time delta between end_date and reference_date is 24 hours")
+
+        current_date = start_date
+
+        output = []
+
+        while int((end_date - current_date).total_seconds()) > minimum_pass_time_sec:
+            sat_pass = self.next_available_pass(satellite, current_date, end_date, minimum_pass_time_sec)
+            if len(sat_pass) == 0:
+                return output
+
+            output.append(sat_pass)
+            current_date = datetime.strptime(sat_pass[-1]['date'], utils.PYEPHEM_DATE_PATTERN)
+
+        return output
+
+    def next_available_passes(self, satellites, start_date: datetime, end_date: datetime,
+                              minimum_pass_time_sec: int = 10) -> dict:
+        """
+        Returns, per satellite, all availability windows between start_date and end_date where
+        there are at least minimum_pass_time_sec position samples with elevation above self.minimum_elevation
+        """
+
+        delta = int((end_date - start_date).total_seconds())
 
         if delta < 0:
             raise ValueError("end_date cannot be before reference_date")
@@ -81,16 +122,20 @@ class Station:
 
         for satellite in satellites:
             output[satellite.name] = self.next_available_pass(satellite,
-                                                              reference_date,
+                                                              start_date,
                                                               end_date,
                                                               minimum_pass_time_sec)
 
         return output
 
-    def next_available_passes_sequence(self, satellites: list, reference_date: datetime, end_date: datetime,
+    def next_available_passes_sequence(self, satellites: list, start_date: datetime, end_date: datetime,
                                        minimum_pass_time_sec: int = 10) -> list:
 
-        delta = int((end_date - reference_date).total_seconds())
+        """
+        Returns, a sequence of passes to track multiple satellites one after the other.
+        """
+
+        delta = int((end_date - start_date).total_seconds())
 
         if delta < 0:
             raise ValueError("end_date cannot be before reference_date")
@@ -100,7 +145,7 @@ class Station:
 
         output = []
 
-        ref_date = reference_date
+        ref_date = start_date
 
         while int((end_date - ref_date).total_seconds()) > minimum_pass_time_sec \
                 and len(satellites) > 0:
